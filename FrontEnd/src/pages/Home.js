048bcccffc3b1b7b3eba3b3fb3a80ac68c392398
Mak <867409182@qq.com>
2017-08-07 23:01:50 +0800

    完成首页前端

--- FrontEnd/src/pages/Home.js
+++ FrontEnd/src/pages/Home.js
@@ -1,7 +1,38 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {observer, inject} from 'mobx-react';
-import { Button } from 'antd-mobile';
-import {Link} from 'react-router-dom'
+import {Grid, Icon} from 'antd-mobile';
+import '../styles/pages/Home.less';
+import GeneralRepair from '../asset/general_repair.svg';
+import Net from '../asset/net.svg';
+import Feedback from '../asset/feedback.svg';
+
+const data = [{
+    icon: GeneralRepair,
+    title: '通用报修',
+    sub: '集合多种报修项目',
+    color1: '#4de685',
+    color2: '#0cd257'
+}, {
+    icon: Net,
+    title: '宽带报修',
+    sub: '宽带报修入口',
+    color1: '#0cd6ec',
+    color2: '#13b1f8'
+}, {
+    icon: Feedback,
+    title: '建议反馈',
+    sub: '功能不好?来吐槽!',
+    color1: '#fd8741',
+    color2: '#fa4f1d'
+
+}, {
+    icon: Feedback,
+    title: '???',
+    sub: '更多功能敬请期待',
+    color1: '#e8e8e8',
+    color2: '#c5c5c5'
+}];
+
 
 @inject("store")
 @observer
@@ -13,12 +44,25 @@ class Home extends Component {
     }
 
     render() {
-        const {token} =this.store;
+        const {token} = this.store;
         return (
             <div>
-                <h2>Home{token}</h2>
-                <Button>antd-mobile button</Button>
-                <Link to="/about">aaaaa</Link>
+                <Grid data={data}
+                      columnNum={2}
+                      renderItem={dataItem => (
+                          <div className="grid-item">
+                              <div className="icon" style={{
+                                  background: `linear-gradient(${dataItem.color1},${dataItem.color2})`
+                              }}>
+                                  <Icon className="icon-img" type={dataItem.icon} alt="icon"/>
+                              </div>
+                              <div className="icon-title">
+                                  <span className="title">{dataItem.title}</span>
+                                  <span className="sub-title">{dataItem.sub}</span>
+                              </div>
+                          </div>
+                      )}
+                />
             </div>
         );
     }
