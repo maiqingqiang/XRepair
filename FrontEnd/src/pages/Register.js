8268c0887ed9bfe53c559c7cf784f7cf0eeb9804
Mak <867409182@qq.com>
2017-08-12 21:24:43 +0800

    去除app

--- FrontEnd/src/pages/Register.js
+++ FrontEnd/src/pages/Register.js
@@ -6,7 +6,7 @@ import {createForm} from 'rc-form';
 import Qs from 'qs';
 import '../styles/pages/Register.less';
 import Geetest from 'geetest3'
-import $ from 'zepto'
+import $ from 'jquery'
 
 @inject('userStore')
 @observer
